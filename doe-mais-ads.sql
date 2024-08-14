32d834443aa1f4be40de11f234be437e6d6968a3
Iury Franca <iurygfranca@gmail.com>
2024-08-13 20:45:15 -0400

    feat: criar bd do projeto ✨

---
+++ doe-mais-ads.sql
@@ -0,0 +1,58 @@
+create database bd_doe_mais_ads;
+
+use bd_doe_mais_ads;
+
+create table entidade
+(
+    id            int primary key,
+    nome          varchar(50) not null,
+    nome_fantasia varchar(50),
+    cpf           varchar(11),
+    cnpj          varchar(14),
+    email         varchar(50) not null,
+    telefone      varchar(11),
+    created_at    timestamp default current_timestamp
+);
+
+create table item
+(
+    id         int primary key,
+    nome       varchar(50) not null,
+    descricao  varchar(400),
+    created_at timestamp default current_timestamp
+);
+
+create table campanha_doacao
+(
+    id            int primary key,
+    nome          varchar(50) not null,
+    descricao     varchar(400),
+    data_inicio   date        not null,
+    data_fim      date,
+
+    id_criador_fk int         not null,
+    foreign key (id_criador_fk) references entidade (id),
+    created_at    timestamp default current_timestamp
+);
+
+create table doacoes
+(
+    id                       int primary key,
+    descricao                varchar(400),
+    id_entidade_doador_fk    int,
+    foreign key (id_entidade_doador_fk) references entidade (id),
+    id_entidade_recebedor_fk int,
+    foreign key (id_entidade_recebedor_fk) references entidade (id),
+    id_campanha_doacao_fk    int,
+    foreign key (id_campanha_doacao_fk) references campanha_doacao (id)
+);
+
+create table item_doacao
+(
+    id           int primary key,
+    quantidade   int,
+    id_doacao_fk int,
+    foreign key (id_doacao_fk) references doacoes (id),
+    id_item_fk   int,
+    foreign key (id_item_fk) references item (id)
+);
