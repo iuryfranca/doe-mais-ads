8d33d47f3374cb60affbb343228dc8c4fac8b2a8
Iury França <iurygfranca@gmail.com>
2024-08-14 02:02:56 -0400

    style: ajustando css do navbar 💎

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{razor,html,cshtml}', './node_modules/flowbite/**/*.js'],
+  content: ['./node_modules/flowbite/**/*.js', './**/*.{razor,html,cshtml}'],
   theme: {
     extend: {},
   },
