a0b554342d6f5e7972b6f97b698c042eed6d5933
Iury França <iurygfranca@gmail.com>
2024-08-13 23:02:53 -0400

    build: configuração inicial de tailwind css 🛠

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{razor,html,cshtml}', './node_modules/flowbite/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/container-queries'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+    require('flowbite/plugin'),
+  ],
+}
